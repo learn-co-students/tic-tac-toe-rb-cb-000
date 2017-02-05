4738b5e06ae43fb975d0c23c6e0e63fc3e015c2d
Patrick Sandoval <patsand24csu@yahoo.com>
2017-02-05 05:49:03 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,92 @@
+WIN_COMBINATIONS = [
+  [0, 1, 2],  #top row
+  [3, 4, 5],  #middle row
+  [6, 7, 8],  #bottom row
+  [0, 3, 6],  #first vertical row
+  [1, 4, 7],  #second vertical row
+  [2, 5, 8],  #third vertical row
+  [0, 4, 8],  #diagonal 1
+  [2, 4, 6]   #diagonal 2
+]
 
+def play(board)
+  while !over?(board)
+    turn(board)
+  end
+  if won?(board)
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cats Game!"
+  end
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(i)
+  i.to_i - 1
+end
+
+#set board[index] to player letter (X/O)
+def move(array, index, player)
+  array[index] = player
+end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken?(board, index)
+end
+
+def position_taken?(board,index)
+  board[index] == "X" || board[index] == "O"
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  user_input = gets.strip
+  index = input_to_index(user_input)
+  if valid_move?(board,index)
+    move(board,index,current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
+
+def turn_count(board)
+  board.count{|position| position == "X" || position == "O"}
+end
+
+def winner(board)
+  if combo = won?(board)
+    board[combo.first]
+  end
+end
+
+def won?(board)
+  WIN_COMBINATIONS.detect do |combo|
+    board[combo[0]] == board[combo[1]] &&
+    board[combo[1]] == board[combo[2]] &&
+    position_taken?(board, combo[0])
+  end
+end
+
+def full?(board)
+  board.all?{|position| position == "X" || position == "O"}
+end
+
+def draw?(board)
+  !won?(board) && full?(board)
+end
+
+def over?(board)
+  won?(board) || draw?(board)
+end
