5321342c4cc81270de6fd9c56a0e0d6f040169af
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-05 05:19:22 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -79,7 +79,7 @@ end
 def won?(board)
 
   # Define your WIN_COMBINATIONS constant
-  win_combo = [[0,1,2], [3,4,5], [6,7,8], [0,4,8],
+  WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,4,8],
   [2,4,6], [0,3,6], [1,4,7], [2,5,8]]
 
    match = board.size.times.select{|i| board[i] == "X"}
