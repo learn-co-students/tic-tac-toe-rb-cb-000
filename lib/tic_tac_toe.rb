ed118d3d5090f8d6e820a85fd2062f3b3fa7a693
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-05 05:02:05 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -77,6 +77,9 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,4,8], 
+[2,4,6], [0,3,6], [1,4,7], [2,5,8]]
 
 def won?(board)
 
@@ -143,9 +146,6 @@ def play(board)
   end
 end
 
-# Define your WIN_COMBINATIONS constant
-WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,4,8], 
-[2,4,6], [0,3,6], [1,4,7], [2,5,8]]
 
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 display_board(board)
