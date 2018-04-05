6249860c3aa91c575f78aebc21dd9647f91b6f4f
Jeffrey Liu <jeffreyliu0@gmail.com>
2018-04-05 05:19:38 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,3 +1,7 @@
+# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,4,8],
+[2,4,6], [0,3,6], [1,4,7], [2,5,8]]
+
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(arrays)
@@ -77,10 +81,6 @@ end
 
 
 def won?(board)
-
-  # Define your WIN_COMBINATIONS constant
-  WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,4,8],
-  [2,4,6], [0,3,6], [1,4,7], [2,5,8]]
 
    match = board.size.times.select{|i| board[i] == "X"}
    match2 = board.size.times.select{|i| board[i] == "O"}
