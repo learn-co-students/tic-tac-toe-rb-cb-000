777e774f5ae09e15cf7ddd3a7660f1ff8e4f8d51
Yosia Hadisusanto <yosia.hadisusanto@gmail.com>
2018-07-18 16:22:03 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,142 @@
+# Define your WIN_COMBINATIONS constant
+#  0 | 1 | 2
+# -----------
+#  3 | 4 | 5
+# -----------
+#  6 | 7 | 8
+
+WIN_COMBINATIONS = [
+  [0,1,2], 
+  [3,4,5],  
+  [6,7,8],  
+  [0,3,6],  
+  [1,4,7],  
+  [2,5,8],  
+  [0,4,8],  
+  [2,4,6]  
+]
+
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+# code your input_to_index and move method here!
+def input_to_index(input)
+  input.to_i - 1
+end
+
+def move(board, index, char)
+  board[index] = char
+end
+
+
+# code your #valid_move? method here
+def valid_move?(board, index)
+  if !index.between?(0,8)
+    false
+  elsif position_taken?(board, index)
+    false
+  else
+    true
+  end
+end
+
+# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+  end
+end
+
+def turn_count(board)
+  count = 0
+  board.each do |item|
+    if item == "O" || item == "X"
+        count += 1
+    end
+  end
+  count
+end
+
+def current_player(board)
+  if turn_count(board) % 2 == 0
+    "X"
+  else
+    "O"
+  end
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  answer = gets.to_i
+  index = input_to_index(answer)
+  if valid_move?(board, index)
+    move(board, index, current_player(board))
+    display_board(board)
+  else 
+    turn(board)
+  end
+end
+
+def play(board)
+  while !over?(board)
+    turn(board)
+  end
+  if winner(board) != nil
+    puts "Congratulations #{winner(board)}!"
+  elsif draw?(board)
+    puts "Cat's Game!"
+  end
+
+end
+
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
+    
+    position_1 = board[win_index_1] 
+    position_2 = board[win_index_2] 
+    position_3 = board[win_index_3] 
+  
+    
+    if position_1 == "X" && position_2 == "X" && position_3  =="X"
+      return win_combination 
+    elsif position_1 == "O" && position_2 == "O" && position_3  =="O"
+      return win_combination 
+    end
+  end
+  return false
+end
+
+def full?(board)
+  board.all? do |pos|
+    pos == "X" || pos == "O"
+  end
+end
+
+def draw?(board)
+  !won?(board) && full?(board)
+end
+
+def over?(board)
+  won?(board) != false || draw?(board) || full?(board)
+end
+
+def winner(board)
+  pos =  won?(board)
+  if pos != false
+    board[pos[0]]
+  else
+    nil
+  end
+end
 
